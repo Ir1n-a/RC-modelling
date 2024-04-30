37f31398700e204029e4c42ed62c53cc82530193
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2024-04-30 16:00:21 +0300

    RC ration calculation

---
+++ RC ratio calculation.jl
@@ -0,0 +1,10 @@
+function RC_ratio_calculation(C,n)
+    R=n/C
+    print(R,"")
+end
+
+RC_ratio_calculation(0.001,250)
+
+#= I need to calculate the time constant ratio between two RCs to measure them. I set
+the first one as 1, so the second one has to be the value of the ratio, because d'uh. Yeah,
+slow day...but I needed to calculate this anyway and this is faster so yeah=#
