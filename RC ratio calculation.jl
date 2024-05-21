6be6eedf9030817a38795086120e5382059dfe79
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2024-05-21 23:10:31 +0300

    sure

--- RC ratio calculation.jl
+++ RC ratio calculation.jl
@@ -3,8 +3,13 @@ function RC_ratio_calculation(C,n)
     print(R,"")
 end
 
-RC_ratio_calculation(0.001,250)
+function Reactance(f,C)
+    Xc=1/(2*π*f*C)
+    print(Xc," ")
+end
 
+RC_ratio_calculation(0.47,10)
+Reactance(0.1,0.0047)
 #= I need to calculate the time constant ratio between two RCs to measure them. I set
 the first one as 1, so the second one has to be the value of the ratio, because d'uh. Yeah,
 slow day...but I needed to calculate this anyway and this is faster so yeah=#
