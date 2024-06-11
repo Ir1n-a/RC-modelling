a30c23609886d86426639b6f1b293ab822604bf4
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2024-06-11 22:47:12 +0300

    ?

--- Impedance Graphs& RC.jl
+++ Impedance Graphs& RC.jl
@@ -2,6 +2,7 @@ using Plots
 using CSV
 using NativeFileDialog
 using DataFrames
+using LsqFit
 plotlyjs()
 
 function plot_format(x,y)
@@ -10,6 +11,24 @@ function plot_format(x,y)
     formatter=:plain,
     top_margin=5*Plots.mm)
 end
+
+function curvy(x,p)
+    r=p[1]
+    x0=p[2]
+    y0=p[3]
+    [(x[1]-x0)^2 + (x[2]-y0)^2 - r^2]
+end
+ff=pick_file()
+df=CSV.read(ff,DataFrame)
+Zre=df."Z' (Ω)"
+Zimg=df."-Z'' (Ω)"
+lb=[0,0,0]
+ub=[maximum(Zre),maximum(Zimg),maximum(Zre)/2]
+p0_bounds=[maximum(Zre)/2,maximum(Zimg)/2,1]
+
+(x.-Zre).^2 + (y.-Zimg).^2 -r^2 
+
+c=curve_fit(curvy, Zre, Zimg, p0_bounds, lower=lb, upper=ub) 
 
 function pick_your_poison(name)
     ff=pick_file()
@@ -22,6 +41,17 @@ function pick_your_poison(name)
     Z=df."Z (Ω)"
     Phase=df."-Phase (°)"
     Time="Time (s)"
+    #R=((Zre.+500).^2).+(Zimg.^2)
+    #p_R=scatter(f,R,xscale=:log10)
+    lb=[0,0,0]
+    ub=[maximum(Zre),maximum(Zimg),maximum(Zre)/2]
+    p0_bounds=[maximum(Zre)/2,maximum(Zimg)/2,1]
+
+
+    c=curve_fit(curvy, Zre, Zimg, p0_bounds, lower=lb, upper=ub)
+    
+
+    savefig(p_R,ff*"_"*name*"Circleqm.html")
 
     p_module=plot_format(f,Z)
     savefig(p_module,ff*"_"*name*"Module.html")
@@ -30,4 +60,4 @@ function pick_your_poison(name)
 
 end
 
-pick_your_poison("P130_full_EIS")
+pick_your_poison("1kohm p 1 mF")
