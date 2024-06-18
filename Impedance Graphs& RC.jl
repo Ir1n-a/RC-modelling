b3c9e0bc009363db892d27bf1df6891dd055002f
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2024-06-18 14:50:56 +0300

    graph

--- Impedance Graphs& RC.jl
+++ Impedance Graphs& RC.jl
@@ -3,18 +3,22 @@ using CSV
 using NativeFileDialog
 using DataFrames
 using LsqFit
+using StatsBase
 plotlyjs()
 
-function plot_format(x,y)
-    plot(x,y,xscale=:log10,
+function plot_format(x,y,sc,xl,yl)
+    max=maximum(x)
+    min=minimum(x)
+    plot(x,y,xscale=sc,
     framestyle=:box,right_margin=7*Plots.mm,linewidth=4,
     formatter=:plain,
-    top_margin=5*Plots.mm)
+    top_margin=5*Plots.mm,xlabel=xl,ylabel=yl,legend=false,
+    ylims=(0,maximum(y)+maximum(y)/10))
 end
 
 function curvy(x,p)
-    r=p[1]
-    x0=p[2]
+    r=p[1] 
+    x0=p[2] 
     y0=p[3]
     [(x[1]-x0)^2 + (x[2]-y0)^2 - r^2]
 end
@@ -48,16 +52,25 @@ function pick_your_poison(name)
     p0_bounds=[maximum(Zre)/2,maximum(Zimg)/2,1]
 
 
-    c=curve_fit(curvy, Zre, Zimg, p0_bounds, lower=lb, upper=ub)
+    #c=curve_fit(curvy, Zre, Zimg, p0_bounds, lower=lb, upper=ub)
     
 
-    savefig(p_R,ff*"_"*name*"Circleqm.html")
+    #savefig(p_R,ff*"_"*name*"Circleqm.html")
 
-    p_module=plot_format(f,Z)
-    savefig(p_module,ff*"_"*name*"Module.html")
-    p_N=plot_format(Zre,Zimg)
-    savefig(p_N,ff*"_"*name*"Nyquist.html")
+    p_module=plot_format(f,Z,:log10,"Frequency (Hz)","Z (Ω)")
+   
+    savefig(p_module,ff*"_"*name*" Module.html")
+
+    p_N=plot_format(Zre,Zimg,:identity,"Zre (Ω)","Zimg (Ω)")
+    savefig(p_N,ff*"_"*name*" Nyquist.html")
+
+    p_B=plot_format(f,Phase,:log10,"Frequency (Hz)","Phase Difference (deg)")
+    savefig(p_B,ff*"_"*name*" Bode.html")
+    
+    D=diff(Z)
+    Dp=plot(midpoints(f),D,xscale=:log10)
+    savefig(Dp,ff*"_Diff_Module.html")
 
 end
 
-pick_your_poison("1kohm p 1 mF")
+pick_your_poison("220 ohm p 1 mF")  
